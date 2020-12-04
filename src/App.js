924b37543ae3c6627b8878fd0fbf4d979321cf56
gzigue <gabriel.zigue@adp.com>
2020-12-04 04:31:08 -0300

    arrozdeleite

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import pave from './arrozdeleite.jpg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <br/>
+        <img src={pave} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <a
+            className="App-link"
+            href="arrozdeleite@arrozdeleite.com.br"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            arrozdeleite@arrozdeleite.com.br
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
